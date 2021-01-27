1f3bdb0ee93ef7b640490709baa1b14555cb4ea1
juanmacivico87 <juanmacivico87@gmail.com>
2021-01-27 17:12:53 +0100

    Add debug, cache, maintenance, performance and security constants for WordPress congiguration file

--- wp-cli.sh
+++ wp-cli.sh
@@ -32,10 +32,34 @@ wp core download --locale=es_ES
 # Create wp-config.php
 wp config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PASSWORD" --dbhost="$DB_HOST:$DB_PORT" --dbcharset="$DB_CHARSET" --dbcollate="$DB_COLLATE" --dbprefix="$DB_PREFIX"
 wp config set WP_ENVIRONMENT_TYPE $WP_ENVIRONMENT_TYPE
-wp config set WP_DEBUG $WP_DEBUG --raw
-wp config set WP_DEBUG_DISPLAY $WP_DEBUG_DISPLAY --raw
+
 wp config set WP_HOME $WP_HOME
 wp config set WP_SITEURL $WP_SITEURL
+
+wp config set WP_DEBUG $WP_DEBUG --raw
+wp config set WP_DEBUG_DISPLAY $WP_DEBUG_DISPLAY --raw
+wp config set WP_DEBUG_LOG $WP_DEBUG_LOG --raw
+wp config set SCRIPT_DEBUG $SCRIPT_DEBUG --raw
+wp config set WP_DISABLE_FATAL_ERROR_HANDLER $WP_DISABLE_FATAL_ERROR_HANDLER --raw
+wp config set IMPORT_DEBUG $IMPORT_DEBUG --raw
+
+wp config set WP_CACHE $WP_CACHE --raw
+wp config set CONCATENATE_SCRIPTS $CONCATENATE_SCRIPTS --raw
+wp config set COMPRESS_CSS $COMPRESS_CSS --raw
+wp config set COMPRESS_SCRIPTS $COMPRESS_SCRIPTS --raw
+wp config set ENFORCE_GZIP $ENFORCE_GZIP --raw
+
+wp config set AUTOMATIC_UPDATER_DISABLED $AUTOMATIC_UPDATER_DISABLED --raw
+wp config set DISALLOW_FILE_EDIT $DISALLOW_FILE_EDIT --raw
+wp config set WP_POST_REVISIONS $WP_POST_REVISIONS --raw
+wp config set EMPTY_TRASH_DAYS $EMPTY_TRASH_DAYS --raw
+wp config set DISABLE_WP_CRON $DISABLE_WP_CRON --raw
+wp config set WP_MEMORY_LIMIT $WP_MEMORY_LIMIT
+wp config set WP_MAX_MEMORY_LIMIT $WP_MAX_MEMORY_LIMIT
+wp config set IMAGE_EDIT_OVERWRITE $IMAGE_EDIT_OVERWRITE --raw
+
+wp config set WP_ALLOW_REPAIR $WP_ALLOW_REPAIR --raw
+wp config set DISABLE_NAG_NOTICES $DISABLE_NAG_NOTICES --raw
 # Install WordPress
 wp core install --url="$WP_HOME" --title="$TITLE" --admin_user="$ADMIN_USER" --admin_password="$ADMIN_PASSWORD" --admin_email="$ADMIN_EMAIL"
 
